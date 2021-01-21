694beefa6923b70fda0cb49de531e71a5254d86b
dohuucong <60845588+dohuucong@users.noreply.github.com>
2021-01-21 11:03:23 +0700

    first commit

---
+++ HRM_Practice/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using HRM_Practice.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRM_Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private HRMRepository repository;
+        public int PageSize = 4;
+
+        public HomeController(HRMRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index() => View(repository.Employees);
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
